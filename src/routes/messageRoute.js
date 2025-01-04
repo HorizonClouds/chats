19a725f10925a85f9b36ff288c346cb5874325a1
JaviFdez7 <javidelatorre400@gmail.com>
2025-01-04 23:53:12 +0100

    feat: implement JWT authentication middleware and update message routes

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,24 +1,13 @@
 import express from 'express';
 import * as messageController from '../controllers/messageController.js';
 import * as messageValidator from '../middlewares/messageValidator.js';
+import { checkAuth, checkPlan, checkRole, checkAddon } from '../middlewares/authMiddelwares.js';
 
 const router = express.Router();
 
-router.post(
-  '/message', 
-  messageValidator.validateCreateMessage, 
-  messageController.createMessage
-);
-router.put('/message/messageStatus/:id', 
-  messageController.updateMessageStatus
-);
-router.delete('/message/:id', messageController.deleteMessage);
-router.get(
-  '/chat/:writerUserId/:receiverUserId', 
-  messageController.getChatBetweenUsersByWriterUserIdAndReceiverUserId
-);
-
-
-
+router.post('/message', checkAuth(), messageValidator.validateCreateMessage, messageController.createMessage);
+router.put('/message/messageStatus/:id', checkAuth(), messageController.updateMessageStatus);
+router.delete('/message/:id', checkAuth(), messageController.deleteMessage);
+router.get('/chat/:writerUserId/:receiverUserId', checkAuth(), messageController.getChatBetweenUsersByWriterUserIdAndReceiverUserId);
 
 export default router;
